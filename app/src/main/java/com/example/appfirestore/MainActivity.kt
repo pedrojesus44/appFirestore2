0bddc4bb02599f4ac25e0fe22961892c6f85e35b
pedrojesus44 <pedro.jesus44@etec.sp.gov.br>
2023-06-14 20:24:53 -0300

    função select

--- app/src/main/java/com/example/appfirestore/MainActivity.kt
+++ app/src/main/java/com/example/appfirestore/MainActivity.kt
@@ -9,14 +9,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.appfirestore.ui.theme.AppFirestoreTheme
 import androidx.compose.material3.*
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.firestore.ktx.firestore
 
 
 class MainActivity : ComponentActivity() {
@@ -29,10 +32,23 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview
+
 @Composable
 fun App() {
+    val navController = rememberNavController()
+
+    NavHost(navController, startDestination = "cadastro") {
+        composable("cadastro") {
+            CadastroScreen(navController)
+        }
+        composable("consulta") {
+            ConsultaScreen(navController)
+        }
+    }
+}
+
+@Composable
+fun CadastroScreen(navController: NavHostController) {
     val db = FirebaseFirestore.getInstance()
 
     val nome = remember { mutableStateOf("") }
@@ -113,5 +129,155 @@ fun App() {
         ) {
             Text("Cadastrar")
         }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("consulta")
+            },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text("Ir para Consulta")
+        }
     }
 }
+
+@Composable
+fun ConsultaScreen(navController: NavHostController) {
+    val db = FirebaseFirestore.getInstance()
+
+    val usuarios = remember { mutableStateListOf<HashMap<String, String>>() }
+    val index = remember { mutableStateOf(0) }
+
+    fun carregarDados() {
+        db.collection("usuario")
+            .get()
+            .addOnSuccessListener { result ->
+                usuarios.clear()
+                for (document in result) {
+                    val userData = hashMapOf<String, String>()
+                    for ((key, value) in document.data) {
+                        userData[key] = value.toString()
+                    }
+                    usuarios.add(userData)
+                }
+
+                // Se a lista de usuários não estiver vazia, definir o índice para o primeiro registro
+                if (usuarios.isNotEmpty()) {
+                    index.value = 0
+                }
+            }
+            .addOnFailureListener { exception ->
+                println("Erro ao carregar dados: $exception")
+            }
+    }
+
+
+    @Composable
+    fun mostrarDados() {
+        val usuarioAtual = usuarios.getOrNull(index.value)
+
+        usuarioAtual?.let {
+            val nomeValue = it["nome"] ?: ""
+            val enderecoValue = it["endereco"] ?: ""
+            val bairroValue = it["bairro"] ?: ""
+            val cepValue = it["cep"] ?: ""
+            val cidadeValue = it["cidade"] ?: ""
+            val estadoValue = it["estado"] ?: ""
+
+            TextField(
+                value = nomeValue,
+                onValueChange = { },
+                label = { Text("Nome") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            TextField(
+                value = enderecoValue,
+                onValueChange = { },
+                label = { Text("Endereço") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            TextField(
+                value = bairroValue,
+                onValueChange = { },
+                label = { Text("Bairro") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            TextField(
+                value = cepValue,
+                onValueChange = { },
+                label = { Text("CEP") },
+                modifier = Modifier.fillMaxWidth(),
+            )
+            TextField(
+                value = cidadeValue,
+                onValueChange = { },
+                label = { Text("Cidade") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            TextField(
+                value = estadoValue,
+                onValueChange = { },
+                label = { Text("Estado") },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+
+    Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "App Firebase - método select",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        mostrarDados()
+
+        Button(
+            onClick = {
+                if (index.value > 0) {
+                    index.value--
+                }
+            },
+            enabled = index.value > 0, // Desabilitar o botão se estiver no primeiro registro
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text("Anterior")
+        }
+
+        Button(
+            onClick = {
+                if (index.value < usuarios.size - 1) {
+                    index.value++
+                }
+            },
+            enabled = index.value < usuarios.size - 1, // Desabilitar o botão se estiver no último registro
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text("Próximo")
+        }
+
+        Button(
+            onClick = {
+                carregarDados()
+            },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text("Carregar Dados")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("cadastro")
+            },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text("Voltar para cadastro")
+        }
+    }
+}
+
+
+
